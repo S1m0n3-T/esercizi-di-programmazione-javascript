cfb2ffbef298b880ff43b7ae097e0cb246787d61
S1m0n3-T <91430548+S1m0n3-T@users.noreply.github.com>
2021-09-26 22:48:09 +0200

    Update somma-e-media.js

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -8,3 +8,16 @@
   
   http://www.imparareaprogrammare.it
 */
+
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+
+var sum = a+b+c+d+e
+var med = sum / 5
+
+
+console.log(sum);
+console.log(med);
